4ddc7015aa9853bef3b2dc47ddf1548020994e6e
kainhuck <35602940+kainhuck@users.noreply.github.com>
2023-09-28 11:10:47 +0800

    update

--- video-maker/ffmpeg.go
+++ video-maker/ffmpeg.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 	"video-delivery/utils"
 )
 
@@ -20,7 +21,12 @@ func (f *Ffmpeg) MergeImageAudio(imageFile, audioFile string) (videoFile string,
 		return videoFile, nil
 	}
 
-	cmd := exec.Command("ffmpeg", "-i", imageFile, "-i", audioFile, "-c:v", "libx264", "-c:a", "aac", "-strict", "experimental", videoFile)
+	duration, err := getMediaDuration(audioFile)
+	if err != nil {
+		return "", err
+	}
+
+	cmd := exec.Command("ffmpeg", "-loop", "1", "-i", imageFile, "-i", audioFile, "-c:v", "libx264", "-t", fmt.Sprintf("%.2f", duration), "-pix_fmt", "yuv420p", videoFile)
 	cmd.Stderr = os.Stderr
 
 	if err := cmd.Run(); err != nil {
@@ -29,4 +35,19 @@ func (f *Ffmpeg) MergeImageAudio(imageFile, audioFile string) (videoFile string,
 	}
 
 	return videoFile, nil
+}
+
+// 获取媒体文件的时长（以秒为单位）
+func getMediaDuration(filename string) (float64, error) {
+	cmd := exec.Command("ffprobe", "-v", "error", "-show_entries", "format=duration", "-of", "default=noprint_wrappers=1:nokey=1", filename)
+	output, err := cmd.Output()
+	if err != nil {
+		return 0, err
+	}
+
+	durationStr := strings.TrimSpace(string(output))
+	duration := 0.0
+	fmt.Sscanf(durationStr, "%f", &duration)
+
+	return duration, nil
 }
